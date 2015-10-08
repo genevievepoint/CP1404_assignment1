18c14487d0cb4d4cf13e6982055c3b789ce02de3
genevievepoint <genevieve.point@my.jcu.edu.au>
2015-10-08 22:52:59 +1000

    update of currency

--- currency.py
+++ currency.py
@@ -1,17 +1,23 @@
 __author__ = 'Genevieve'
 
-from trip import Error
-from trip import Country
-from trip import Details
 from web_utility import load_page
 
+open('currency_details.txt', encoding='utf-8')
 
 
-def convert(amount, home_currency, location_currency_code):
+# def convert(amount, home_currency, location_currency_code):
 
 
 
 
-
-def get_details(country_name)
-
+def get_details(country_name):
+    country = input("Please enter a country ")
+    found = False
+    in_file = open("currency_details.txt", encoding='utf-8')
+    for line in in_file:
+        parts = line.strip().split(',')
+    if parts[0] == country:
+        found = True
+        print("In ", parts[0], "the currency is ", parts[1], "and the symbol is", parts[2])
+    if found == False:
+        print('Country has gone missing!')
--- trip.py
+++ trip.py
@@ -1,14 +1,16 @@
 __author__ = 'Genevieve'
 
 
-class Error:
-    def
+ open('currency_details.txt', encoding='utf-8')
 
-class Country:
-    def __init__(self, name="", currency_code="", currency_symbol=""):
-        self.name = name
-        self.currency_code = currency_code
-        self.currency_symbol = currency_symbol
+# class Error:
+#     def
+#
+ class Country:
+     def __init__(self, name="", currency_code="", currency_symbol=""):
+         self.name = name
+         self.currency_code = currency_code
+         self.currency_symbol = currency_symbol
 
 
     def currency_string(self):
@@ -18,22 +20,21 @@ class Country:
     def __str__(self):
         return "self.name, self.currency_code, self.currency_symbol"
 
-
-class Details:
-    def __init__ (self, locations=""):
-        self.locations = locations
-
-
-    def add(self, country_name, start_date, end_date):
-        self.country_name = country_name
-        self.start_date = start_date
-        self.end_date = end_date
-
-
-    def current_country(self, date_string):
-        self.date_string = date_string
-        return
-
-    def is_empty(self):
+# class Details:
+#     def __init__ (self, locations=""):
+#         self.locations = locations
+#
+#
+#     def add(self, country_name, start_date, end_date):
+#         self.country_name = country_name
+#         self.start_date = start_date
+#         self.end_date = end_date
+#
+#
+#     def current_country(self, date_string):
+#         self.date_string = date_string
+#         return
+#
+#     def is_empty(self):
 
 
