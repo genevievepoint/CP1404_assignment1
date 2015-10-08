3e2a3bea1d32c68338c59680da9fcafe17e7c79e
genevievepoint <genevieve.point@my.jcu.edu.au>
2015-10-08 12:48:29 +1000

    Start of assignment

---
+++ currency.py
@@ -0,0 +1,17 @@
+__author__ = 'Genevieve'
+
+from trip import Error
+from trip import Country
+from trip import Details
+from web_utility import load_page
+
+
+
+def convert(amount, home_currency, location_currency_code):
+
+
+
+
+
+def get_details(country_name)
+
---
+++ trip.py
@@ -0,0 +1,39 @@
+__author__ = 'Genevieve'
+
+
+class Error:
+    def
+
+class Country:
+    def __init__(self, name="", currency_code="", currency_symbol=""):
+        self.name = name
+        self.currency_code = currency_code
+        self.currency_symbol = currency_symbol
+
+
+    def currency_string(self):
+        return "{}, self.currency_symbol{:.2f}".format(super().__str__(), self.currency_code, self.name)
+
+
+    def __str__(self):
+        return "self.name, self.currency_code, self.currency_symbol"
+
+
+class Details:
+    def __init__ (self, locations=""):
+        self.locations = locations
+
+
+    def add(self, country_name, start_date, end_date):
+        self.country_name = country_name
+        self.start_date = start_date
+        self.end_date = end_date
+
+
+    def current_country(self, date_string):
+        self.date_string = date_string
+        return
+
+    def is_empty(self):
+
+
