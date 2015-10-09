955c6d88b578cdf67cd7d2f82fe8015dc5d61a9d
genevievepoint <genevieve.point@my.jcu.edu.au>
2015-10-09 15:33:51 +1000

    another update

--- currency.py
+++ currency.py
@@ -1,23 +1,31 @@
 __author__ = 'Genevieve'
 
-from web_utility import load_page
+import web_utility
+from trip import Country
+from trip import Details
 
 open('currency_details.txt', encoding='utf-8')
 
 
-# def convert(amount, home_currency, location_currency_code):
+def convert(amount, home_currency, location_currency_code):
+    amount = input("please enter the amount you wish to convert")
+    home_currency = input("Please enter your current currency")
+    location_currency_code = input("Please enter where you are going")
+    url_string = 'https://www.google.com/finance/converter?a=&from=&to='
+    result = web_utility.load_page(url_string)
+    result = self.currency_symbol()
+    print(result[result.index('result'):])
 
 
 
-
-def get_details(country_name):
-    country = input("Please enter a country ")
-    found = False
-    in_file = open("currency_details.txt", encoding='utf-8')
-    for line in in_file:
-        parts = line.strip().split(',')
-    if parts[0] == country:
-        found = True
-        print("In ", parts[0], "the currency is ", parts[1], "and the symbol is", parts[2])
-    if found == False:
-        print('Country has gone missing!')
+# def get_details(country_name):
+#     country_name = input("Please enter a country ")
+#     found = False
+#     in_file = open("currency_details.txt", encoding='utf-8')
+#     for line in in_file:
+#         parts = line.strip().split(',')
+#     if parts[0] == country_name:
+#         found = True
+#         print(parts[0], parts[1], parts[2])
+#     if found == False:
+#         print('', ' , ', '', ' , ',  '')
--- trip.py
+++ trip.py
@@ -1,16 +1,14 @@
 __author__ = 'Genevieve'
 
+class Error:
+    def exceptions(self):
 
- open('currency_details.txt', encoding='utf-8')
 
-# class Error:
-#     def
-#
- class Country:
-     def __init__(self, name="", currency_code="", currency_symbol=""):
-         self.name = name
-         self.currency_code = currency_code
-         self.currency_symbol = currency_symbol
+class Country:
+    def __init__(self, name="", currency_code="", currency_symbol=""):
+        self.name = name
+        self.currency_code = currency_code
+        self.currency_symbol = currency_symbol
 
 
     def currency_string(self):
@@ -20,21 +18,29 @@ __author__ = 'Genevieve'
     def __str__(self):
         return "self.name, self.currency_code, self.currency_symbol"
 
-# class Details:
-#     def __init__ (self, locations=""):
-#         self.locations = locations
-#
-#
-#     def add(self, country_name, start_date, end_date):
-#         self.country_name = country_name
-#         self.start_date = start_date
-#         self.end_date = end_date
-#
-#
-#     def current_country(self, date_string):
-#         self.date_string = date_string
-#         return
-#
-#     def is_empty(self):
 
+
+class Details:
+    def __init__ (self, locations=""):
+        self.locations = locations
+
+
+    def add(self, country_name, start_date, end_date):
+        self.country_name = country_name
+        self.start_date = start_date
+        self.end_date = end_date
+
+
+    def current_country(self, date_string):
+        self.date_string = date_string
+        return
+
+    def is_empty(self):
+
+
+<<<<<< Updated upstream
+
+=======
+#
+>>>>>>> Stashed changes
 
