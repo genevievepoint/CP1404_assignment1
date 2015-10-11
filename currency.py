ac77bb1f8a4c3cfd9bbb78181ae2efa9bf9ae9cb
genevievepoint <genevieve.point@my.jcu.edu.au>
2015-10-11 21:09:31 +1000

    another update 5

--- currency.py
+++ currency.py
@@ -1,51 +1,69 @@
 __author__ = 'Genevieve'
 
-import re
-from web_utility import load_page
-from trip import Country
-from trip import Details
-from trip import Error
+import web_utility
 
 open('currency_details.txt', encoding='utf-8')
 
 
 def convert(amount, home_currency, location_currency_code):
-    amount = input("please enter the amount you wish to convert")
-    if amount <= 0:
-        print('-1')
-    else:
-        amount = amount
+    url_string = "https://www.google.com/finance/converter?a={}&from={}&to={}".format(amount, home_currency, location_currency_code)
 
-    home_currency = get_details()
-    location_currency_code = get_details()
-    url = 'https://www.google.com/finance/converter'
-    values = {'a': amount, 'from': home_currency, 'to': location_currency_code}
-    data = urllib.parse.urlopen(values)
-    data = data.encode('utf-8')
-    req = urllib.request.Request(url, data)
-    resp = urllib.request.urlopen(req)
-    respData = resp.read()
-
-    # print(respData)
-
-    paragraphs = re.findall('r'<p>()</p>,str(respData))
-
-    for eachP in paragraphs:
-        print(eachP)
-
-    # url_string = url
-    # result = web_utility.load_page(url_string)
-    # print(result[result.index('result'):])
+    result = web_utility.load_page(url_string)
+    result = result[result.index('result'):]
 
 
-def get_details(country_name):
-    country_name = input("Please enter a country ")
-    found = False
-    in_file = open("currency_details.txt", encoding='utf-8')
-    for line in in_file:
-        parts = line.strip().split(',')
-    if parts[0] == country_name:
-        found = True
-        return(parts[1])
-    if found == False:
-         print('', ' , ', '', ' , ',  '')
+    split_result = result.split('>')[2]
+    conversion_amount = split_result.split(' ')[0]
+
+    if conversion_amount == '\\n</div':
+        return '-1.00'
+    return conversion_amount
+
+
+def details(country_name):
+    in_file = open("currency_details.txt", 'r', encoding='utf-8')
+    lines = in_file.readline()
+    if country_name == '':
+        return tuple()
+    for line in lines:
+        if country_name in line:
+            line = line.strip().split(',')
+            line_info = line[0], line[1], line[2]
+            return tuple(line_info)
+        else:
+            return tuple()
+        # parts = line.strip().split(',')
+        # if parts[0] == country_name:
+        #     found = True
+        # return(parts[0], parts[1], parts[2])
+    # return()
+
+
+# test to see if this stuff works
+
+
+def main():
+    amount = [1.00, 1.00, 1.00, 10.95, 943.18, 10.95, 13.62, 200.15, 13859.49, 100.00, 0.83, 19.99, 34.58, 19.99, 27.80]
+    home_currency_code = ['AUD', 'JPY', 'ABC', 'AUD', 'JPY', 'AUD', 'BGN', 'BGN', 'JPY', 'JPY', 'USD', 'USD', 'BGN', 'USD', 'AUD']
+    location_currency_code = ['AUD', 'ABC', 'USD', 'JPY', 'AUD', 'BGN', 'AUD', 'JPY', 'BGN', 'USD', 'JPY', 'BGN', 'USD', 'AUD', 'USD']
+
+    for i in range(0,15):
+        test = convert(amount[i], home_currency_code[i], location_currency_code[i])
+        if test == '-1.00':
+            valid = 'invalid conversion'
+        else:
+            valid = 'valid conversion'
+        print(valid, amount[i], home_currency_code[i], ' -> ', location_currency_code[i], test)
+
+    country_name = ('Unknown', 'Japanese', ' ', 'Australia', 'Japan', 'Hong Kong')
+    for i in range(0, 3):
+        print('invalid details ', details(country_name[i]))
+    for i in range(3, 6):
+        print('valid details', details(country_name[i]))
+
+
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
--- trip.py
+++ trip.py
@@ -1,9 +1,10 @@
 __author__ = 'Genevieve'
 
+
 class Error:
- except: (Exception)
- print("Failure to proceed because of: ", Exception)
-        # self.exceptions = exceptions
+    def __init__(self, value, **kwargs):
+        super(Error, self).__init__(value)
+
 
 class Country:
     def __init__(self, name="", currency_code="", currency_symbol=""):
@@ -11,28 +12,30 @@ class Country:
         self.currency_code = currency_code
         self.currency_symbol = currency_symbol
 
-
-    def currency_string(self):
-        return "{}, self.currency_symbol{:.2f}".format(super().__str__(), self.currency_code, self.name)
-
+    def currency_value(self, amount):
+        amount = '{:.2f}'.format(amount)
+        return str(amount)
 
     def __str__(self):
         return "self.name, self.currency_code, self.currency_symbol"
 
 
-
 class Details:
     def __init__ (self, locations=""):
-        self.locations = locations
-
+        self.locations = []
 
     def add(self, country_name, start_date, end_date):
-        self.country_name = country_name
-        self.start_date = start_date
-        self.end_date = end_date
+        self.locations.append((country_name, start_date, end_date))
+        start_date = '[0] / [1] / [2]'.format(start_date[0], start_date[1], start_date[2])
+        end_date = '[0] / [1] / [2]'.format(end_date[0], end_date[1], end_date[2])
+
+        try:
+            if start_day < end_date:
+
 
 
     def current_country(self, date_string):
+        for location in
         self.date_string = date_string
         return
 
